d94312163b2a425a00f6fe5ac775bb1fd831931e
Cynosure159 <15217310106@163.com>
2020-08-05 11:48:23 +0800

    1

---
+++ 管理界面/register.js
@@ -0,0 +1,10 @@
+function getinfo() {
+	var prepasswd = document.getElementById('prepasswd').value;
+	var email = document.getElementById('email').value;
+	var passwd = document.getElementById('passwd').value;
+
+}
+
+function forget() {
+	alert("不会吧不会吧，不会真有人记不住密码的?")
+}
